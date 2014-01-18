e24a22a33b73e8497695b3d6a02503d3bdd76255
Daniel Le <GreenRecycleBin@gmail.com>
2014-01-18 21:54:40 +0800

    Host CrimeFragment in CrimeActivity

--- CriminalIntent/src/main/java/com/greenrecyclebin/android/criminalintent/CrimeActivity.java
+++ CriminalIntent/src/main/java/com/greenrecyclebin/android/criminalintent/CrimeActivity.java
@@ -1,7 +1,9 @@
 package com.greenrecyclebin.android.criminalintent;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
 
 public class CrimeActivity extends FragmentActivity {
 
@@ -9,6 +11,14 @@ public class CrimeActivity extends FragmentActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crime);
+
+        FragmentManager manager = getSupportFragmentManager();
+        Fragment fragment = manager.findFragmentById(R.id.fragmentContainter);
+
+        if (fragment == null) {
+            fragment = new CrimeFragment();
+            manager.beginTransaction().add(R.id.fragmentContainter, fragment).commit();
+        }
     }
 
 }

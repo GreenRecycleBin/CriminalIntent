8be0ff2178cadcfb8a50fc298a6909358814609c
Daniel Le <GreenRecycleBin@gmail.com>
2014-02-22 14:59:48 +0800

    Add a singleton CrimeLab to store a list of Crimes

---
+++ CriminalIntent/src/main/java/com/greenrecyclebin/android/criminalintent/CrimeLab.java
@@ -0,0 +1,40 @@
+package com.greenrecyclebin.android.criminalintent;
+
+import android.content.Context;
+
+import java.util.ArrayList;
+import java.util.UUID;
+
+/**
+ * Created by greenrecyclebin on 22/2/14.
+ */
+public class CrimeLab {
+    private static CrimeLab sCrimeLab;
+
+    private Context mAppContext;
+    private ArrayList<Crime> mCrimes;
+
+    private CrimeLab(Context appContext) {
+        mAppContext = appContext;
+        mCrimes = new ArrayList<>();
+    }
+
+    public static synchronized CrimeLab get(Context c) {
+        if (sCrimeLab == null)
+            sCrimeLab = new CrimeLab(c.getApplicationContext());
+
+        return sCrimeLab;
+    }
+
+    public ArrayList<Crime> getCrimes() {
+        return mCrimes;
+    }
+
+    public Crime getCrime(UUID id) {
+        for (Crime c : mCrimes)
+            if (c.getId().equals(id))
+                return c;
+
+        return null;
+    }
+}
